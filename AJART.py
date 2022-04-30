7d7067fe62d95e243ca62ea4b345092e7c885296
Alphactory <aedan.e.taylor@gmail.com>
2022-04-29 22:57:17 -0400

    Release 1.0.0

---
+++ AJART.py
@@ -0,0 +1,96 @@
+import time
+import tkinter
+import tkinter.messagebox
+import tkinter.font
+from tkinter import *
+from tkinter import filedialog
+import Painter
+import webbrowser
+import threading
+
+
+class AJArt_Gui:
+    def __init__(self):
+        self.win = Tk()
+        self.win.iconphoto(False, tkinter.PhotoImage(file="ajarticon.png"))
+        self.win.title('Animal Jam Art Printer')
+        self.file_path = None
+        self.mode = StringVar(self.win, "CMYK")
+        self.speed = BooleanVar(self.win, True)
+        self.scaling_factor = IntVar(self.win, 10)
+        self.p = None
+        topframe = Frame(self.win, relief=RAISED)
+        topframe.pack(fill="both", expand=True)
+        pad=50
+        topframe.grid_rowconfigure(1, weight=1, pad=pad)
+        topframe.grid_columnconfigure(1, weight=1, pad=pad)
+        topframe.grid_rowconfigure(0, weight=1, pad=pad)
+        topframe.grid_columnconfigure(0, weight=1, pad=pad)
+
+        ###SELECT IMAGE BUTTON###
+        select_image_button = Button(topframe, text="Select Image", width=20, height=5, command=self.select_image)
+        select_image_button.grid(row=0,column=0)
+
+        ###MODE SELECT###
+        modes_frame = Frame(topframe)
+        select_mode_message = Message(modes_frame, text="------Select Mode------", width=300)
+        select_mode_message.pack()
+        mode_dict = {"CMYK": "CMYK", "L": "Grayscale"}
+        for mode in mode_dict.keys():
+            Radiobutton(modes_frame, text=mode_dict[mode], value=mode, var=self.mode).pack()
+        modes_frame.grid(row=0, column=1)
+
+        ###DOTSIZE SELECT###
+        ds_frame = Frame(topframe)
+        select_ds_message = Message(ds_frame, text="------Select Dot Size------", width=300)
+        select_ds_message.pack()
+        ds_dict = {2: "Tiny", 10: "Small", 20:"Medium", 30:"Large", 40:"Very Large", 60:"Gigantic"}
+        for ds in ds_dict.keys():
+            Radiobutton(ds_frame, text=ds_dict[ds], value=ds, var=self.scaling_factor).pack()
+        ds_frame.grid(row=1, column=0)
+
+        ###LOAD BUTTON###
+        load_button = Button(self.win, text="Load", width=20, height=5, command=self.load)
+        load_button.pack()
+
+        ###SPEED SELECT###
+        speed_frame = Frame(topframe)
+        select_speed_message = Message(speed_frame, text="------Select Speed------", width=300)
+        select_speed_message.pack()
+        speed_dict = {True: "Fast", False: "Accurate (No Glitches)"}
+        for speed in speed_dict.keys():
+            Radiobutton(speed_frame, text=speed_dict[speed], value=speed, var=self.speed).pack()
+        speed_frame.grid(row=1, column=1)
+
+        ###BOTTOM MESSAGE###
+        my_font = tkinter.font.Font(size=18)
+        controls_message = Message(self.win, text="Once Loaded, Press '[' to start, and ']' to pause",
+                                   width=550, font=my_font)
+        controls_message.pack(fill="both", expand=True)
+
+        self.win.mainloop()
+
+    def select_image(self):
+        self.file_path = filedialog.askopenfilename()
+
+    def load(self):
+        if not self.file_path is None:
+            threading.Thread(target=self.loadthread, daemon=True).start()
+        else:
+            tkinter.messagebox.showerror("Error", "No Image Selected.")
+
+    def loadthread(self):
+        self.p = Painter.Painter(self.file_path, self.mode.get(), self.scaling_factor.get(), self.speed.get())
+        threading.Thread(target=self.thanks, daemon=True).start()
+
+    def thanks(self):
+        while self.p.done is False:
+            time.sleep(1)
+        result = tkinter.messagebox.askquestion("Thank you", "Thanks for using AJART Printer! Was your print worth supporting me for 5$?")
+        if result == "yes":
+            webbrowser.open("https://paypal.me/AedanETaylor")
+        exit(0)
+
+
+if __name__ == "__main__":
+    main = AJArt_Gui()
---
+++ Painter.py
@@ -0,0 +1,159 @@
+import time
+
+import pyautogui
+import keyboard
+from PIL import Image
+from math import floor
+import tkinter
+
+
+def select_color(color):
+    pyautogui.click(53, 31)
+    time.sleep(1)
+    if color == "cyan":
+        pyautogui.click(457, 510)
+    elif color == "magenta":
+        pyautogui.click(457, 820)
+    elif color == "yellow":
+        pyautogui.click(457, 327)
+    elif color == "key" or color == "black":
+        pyautogui.click(1000, 930)
+    elif color == "red":
+        pyautogui.click(457, 165)
+    elif color == "blue":
+        pyautogui.click(457, 409)
+    elif color == "green":
+        pyautogui.click(457, 695)
+    else:
+        raise ValueError(f"Value {color} not in colors")
+    time.sleep(1)
+    pyautogui.click(1108, 60)
+    time.sleep(1)
+
+def select_size(size):
+    pyautogui.click(53, 516)
+    time.sleep(1)
+    if size <= 2:
+        pyautogui.click(247, 660)
+    elif size <= 10:
+        pyautogui.click(420, 660)
+    elif size <= 20:
+        pyautogui.click(576, 660)
+    elif size <= 30:
+        pyautogui.click(751, 660)
+    elif size <= 40:
+        pyautogui.click(929, 660)
+    else:
+        pyautogui.click(1087, 660)
+    time.sleep(1)
+
+
+def gcr(im):
+    """
+    Basic "Gray Component Replacement" function. Returns a CMYK image with
+    percentage gray component removed from the CMY channels and put in the
+    K channel, ie. for percentage=100, (41, 100, 255, 0) >> (0, 59, 214, 41)
+    """
+    cmyk_im = im.split()
+    cmyk = []
+    for i in range(4):
+        cmyk.append(cmyk_im[i].load())
+    for x in range(im.size[0]):
+        for y in range(im.size[1]):
+            gray = int(
+                min(cmyk[0][x, y], cmyk[1][x, y], cmyk[2][x, y])
+            )
+            for i in range(3):
+                cmyk[i][x, y] = cmyk[i][x, y] - gray
+            cmyk[3][x, y] = gray
+    return Image.merge("CMYK", cmyk_im)
+
+
+def getcolors(mode):
+    if mode == "CMYK":
+        return ["cyan", "magenta", "yellow", "key"]
+    elif mode == "L":
+        return ["black"]
+    elif mode == "RGB":
+        return ["red", "green", "blue"]
+
+
+def click(xloc, yloc, speed):
+    if speed:
+        pyautogui.click(xloc, yloc)
+    else:
+        pyautogui.moveTo(xloc, yloc)
+        pyautogui.mouseDown()
+        pyautogui.mouseUp()
+
+
+class Painter:
+    def __init__(self, filename, mode, scaledivisor, speed=True):
+        self.speed = speed
+        self.done = False
+        self.filename = filename
+        self.mode = mode
+        self.scaledivisor = scaledivisor
+        self.img = Image.open(filename)
+        self.img = self.img.convert(self.mode)
+        if self.mode == "CMYK":
+            self.img = gcr(self.img)
+        self.width = 1320
+        self.height = 1080
+        self.img = self.img.resize((self.width // self.scaledivisor, self.height // self.scaledivisor))
+        self.pix = self.img.load()
+        self.x, self.y = self.img.size
+        print(self.x, self.y)
+        self.img.show()
+        keyboard.add_hotkey("[", self.drawpic)
+
+    def drawpic(self, colorrange=20):
+        pyautogui.click(71, 414)
+        time.sleep(1)
+        pyautogui.click(412, 216)
+        time.sleep(1)
+        pyautogui.click(39, 883)
+        time.sleep(1)
+        pyautogui.click(1140, 643)
+        time.sleep(1)
+        select_size(self.scaledivisor)
+
+        keyboard.unhook_all_hotkeys()
+        stop = False
+        counter = 0
+        for color in getcolors(self.mode):
+            if stop:
+                while not keyboard.is_pressed("["):
+                    pass
+                stop = False
+                print("unstopped")
+            select_color(color)
+            for xval in range(self.x):
+                if stop:
+                    while not keyboard.is_pressed("["):
+                        pass
+                    stop = False
+                    print("unstopped")
+                for yval in range(self.y):
+                    if stop:
+                        while not keyboard.is_pressed("["):
+                            pass
+                        stop = False
+                        print("unstopped")
+                    xloc = 360 + xval * self.width / self.x
+                    yloc = yval * self.height / self.y
+                    px = self.pix[xval, yval]
+                    print(px)
+                    if type(px) != int:
+                        pixel = px[counter]
+                    else:
+                        pixel = 256 - px
+                    pixmult = floor(pixel * colorrange / 256)
+
+                    for i in range(pixmult):
+                        click(xloc, yloc, self.speed)
+                    if keyboard.is_pressed("]"):
+                        stop = True
+            counter += 1
+        self.done = True
+
